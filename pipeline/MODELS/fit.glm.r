a59b1a846c6c2727d4bbf6e4e8b3b5462aeda976
Anna Norberg <anna.norberg@helsinki.fi>
2018-05-21 13:32:44 +0300

    ML GLM update

--- pipeline/MODELS/fit.glm.r
+++ pipeline/MODELS/fit.glm.r
@@ -2,26 +2,23 @@
 # BASIC UNIVARIATE GENERALIZED LINEAR MODELS
 ##########################################################################################
 
-require("pROC")
-
-##########################################################################################
-
 if (set_no=="birds" | set_no=="butterfly" | set_no=="plant") { 
-	form <- as.formula(sp~IC+V1+V2+V3+V4+V5+V1_2+V2_2+V3_2+V4_2+V5_2)
+	form <- as.formula(sp~1+V1+V2+V3+V4+V5+V1_2+V2_2+V3_2+V4_2+V5_2)
 }
 if (set_no=="trees") { 
-	form <- as.formula(sp~IC+V1+V2+V3+V1_2+V2_2+V3_2)
+	form <- as.formula(sp~1+V1+V2+V3+V1_2+V2_2+V3_2)
 }
 if (set_no=="vegetation") { 
-	form <- as.formula(sp~IC+V1+V2+V3+V4+V1_2+V2_2+V3_2+V4_2)
+	form <- as.formula(sp~1+V1+V2+V3+V4+V1_2+V2_2+V3_2+V4_2)
 }
+
 
 ##########################################################################################
 
 for (j in 1:3) {
 
 	nsp <- length(DD_t[[j]])
-	
+
 	if (j==1) { sT<-Sys.time() }
 		glms1	<-	foreach	(i=1:nsp) %dopar% { tryCatch({ glm(form, family=binomial(link = "probit"), data=DD_t[[j]][[i]])}, 
 						error=function(e){cat("ERROR :",conditionMessage(e), "\n")}) }
@@ -30,9 +27,9 @@ for (j in 1:3) {
 		comTimes<-eT-sT
 	}
 
-	save(glms1, file=paste(FD,set_no,"/glm1_",j,"_",dataN[sz],".RData",sep=""))
+	save(glms1, file=paste(FD2,set_no,"/glm1_",j,"_",dataN[sz],".RData",sep=""))
 	if (j==1) {
-		save(comTimes, file=paste(FD,set_no,"/comTimes_GLM1_",dataN[sz],".RData",sep=""))
+		save(comTimes, file=paste(FD2,set_no,"/comTimes_GLM1_",dataN[sz],".RData",sep=""))
 	}
 }
 
--- pipeline/PREDICT/predict.glm.r
+++ pipeline/PREDICT/predict.glm.r
@@ -1,33 +1,55 @@
 ##########################################################################################
-# BASIC UNIVARIATE GENERALIZED LINEAR MODELS PREDICTIONS
+# BASIC UNIVARIATE GENERALIZED LINEAR MODELS PREDICTIONS (with parameter uncertainty)
+##########################################################################################
+
+# function for incorporating parameter uncertainty
+##########################################################################################
+require(mvtnorm)
+predict.glm.sdf <- function( glm.mod, predX, paramUncert=TRUE, REPs=100, doPlot=FALSE) {
+if (paramUncert==TRUE){
+	vcovy <- vcov( glm.mod) #will this always be available?  Will it be Pos Def?
+    if( !is.element( "sp", colnames( predX)))
+    	predX$sp <- rep( -999, nrow( predX))  #-999 is a dummy placeholder.  Could change na.action too...
+    	X_mod_pred <- model.matrix( glm.mod$formula, data=predX)
+    	ranParams <- t( rmvnorm( REPs, coef( glm.mod), sigma=vcovy))
+    	ranLinPreds <- X_mod_pred %*% ranParams
+    	ranPreds <- glm.mod$family$linkinv( ranLinPreds) #this code will remain constant even if you change the link.
+  	} else {
+  		ranPreds <- predict( glm.mod, type='response', newdata=predX)
+  		ranPreds <- matrix( rep( ranPreds, times=REPs), ncol=REPs)  #OK, the name of the variable is not descriptive here.  It is not random.
+  	}
+  
+  ranY <- matrix( rbinom( n=length( ranPreds), size=1, prob=ranPreds), nrow=nrow( ranPreds), ncol=ncol( ranPreds))
+  	# attributes(ranY)$ranPreds <- ranPreds
+	if (doPlot){
+		matplot( predict( glm.mod, type="response", newdata=predX), ranPreds, pch='.', cex=1.5)
+    	abline(0,1, lwd=2)
+    }
+  return( ranY)
+}
 ##########################################################################################
 
 for (j in 1:3) {
 
-	load(file=paste(FD,set_no,"/glm1_",j,"_",dataN[sz],".RData",sep=""))
+	load(file=paste(FD2,set_no,"/glm1_",j,"_",dataN[sz],".RData",sep=""))
 
 	nsp <- length(DD_v[[j]])
 	nsites <- nrow(DD_v[[j]][[1]])
 
-	glm1_PAs <- array(NA,dim=list(nsites,nsp,REPs))
-
-	glm1_preds<-foreach(i=1:nsp) %dopar% { tryCatch({ predict(glms1[[i]], newdata=DD_v[[j]][[i]], type='response')}, 
-											error=function(e){cat("ERROR :",conditionMessage(e), "\n")}) }
-
-	for (i in 1:nsp) {
-		if(is(glm1_preds[[i]])[1]=="try-error" | is(glm1_preds[[i]])[1]=="NULL"){ glm1_preds[[i]] <-  rep(mean(DD_t[[j]][[i]][,1]),times=nsites) }
-	}
-
-	glm1_preds<-simplify2array(glm1_preds)
-
-	for (n in 1:REPs) {
-		glm1_PAs[,,n]<-rbinom(glm1_preds,1,glm1_preds)
-	}
+	glm1_PAs <- foreach(i=1:nsp)  %dopar% { tryCatch({ predict.glm.sdf(glms1[[i]], predX=DD_v[[j]][[i]], paramUncert=FALSE) },
+	                                                    error=function(e){cat("ERROR :",conditionMessage(e), "\n")}) }
+	glm1_PAs <- aperm(simplify2array( glm1_PAs), c(1,3,2))
 
 	save(glm1_PAs, file=paste(PD2,set_no,"/glm1_PAs_",j,"_",dataN[sz],".RData",sep=""))
 
+	glm1b_PAs <- foreach(i=1:nsp)  %dopar% { tryCatch({ predict.glm.sdf(glms1[[i]], predX=DD_v[[j]][[i]], paramUncert=TRUE) },
+	                                                    error=function(e){cat("ERROR :",conditionMessage(e), "\n")}) }
+	glm1b_PAs <- aperm(simplify2array( glm1b_PAs), c(1,3,2))
+
+	save(glm1b_PAs, file=paste(PD2,set_no,"/glm1b_PAs_",j,"_",dataN[sz],".RData",sep=""))
+
 	rm(glms1)
-	rm(glm1_preds)
+	rm(glm1b_PAs)
 	rm(glm1_PAs)
 	gc()
 
