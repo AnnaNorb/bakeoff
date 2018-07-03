8e7c3a4a6e418331c2c4bd269273ef79c3bb52a0
Anna Norberg <anna.norberg@helsinki.fi>
2018-07-03 20:42:02 +0300

    update vignettes

--- pipeline/SCRIPTS/HMSCpipeline.m
+++ pipeline/SCRIPTS/HMSCpipeline.m
@@ -1,26 +1,16 @@
 clc;
 clearvars;
 
-% run twice longer MCMC chains?
-MCMC2=false;
-
-cd('D:\HY-data\NORBERG\OneDrive - University of Helsinki\bakeoff\pipeline\MODELS')
-%cd('/Users/anorberg/OneDrive - University of Helsinki/bakeoff/pipeline/MODELS')
-
-dsz=3;
-jobBird=batch('jobHMSCbirds')
-jobButter=batch('jobHMSCbutter')
-jobPlant=batch('jobHMSCplant')
-jobVegt=batch('jobHMSCveg')
-jobTrees=batch('jobHMSCtree')
-
 % pipeine directory
-%wdpath=fullfile('...','bakeoff','pipeline');
+wdpath=fullfile('...','bakeoff','pipeline');
 
 % run settings
 bakeoffSettings=fullfile(wdpath,'SCRIPTS','settingsHMSCmatlab.m');
 bakeoff_ssSettings=fullfile(wdpath,'SCRIPTS','settings_ssHMSCmatlab.m');
 run(bakeoffSettings)
+
+% run twice longer MCMC chains?
+MCMC2=false;
 
 for dsz=1:3     % data size (1, 2 or 3)
     for s=1:5       % data set
